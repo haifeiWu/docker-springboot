602d8808aff7e615b424a98df33272ff4d29c417
haifeiWu <whfstudio@163.com>
2019-02-11 18:25:20 +0800

    update

--- src/main/java/com/whforever/dockerspringboot/config/ActuatorSecurityConfig.java
+++ src/main/java/com/whforever/dockerspringboot/config/ActuatorSecurityConfig.java
@@ -12,16 +12,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  **/
 public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    /*
-        This spring security configuration does the following
-
-        1. Restrict access to the Shutdown endpoint to the ACTUATOR_ADMIN role.
-        2. Allow access to all other actuator endpoints.
-        3. Allow access to static resources.
-        4. Allow access to the home page (/).
-        5. All other requests need to be authenticated.
-        5. Enable http basic authentication to make the configuration complete.
-           You are free to use any other form of authentication.
+    /**
+     * This spring security configuration does the following
+     *
+     * 1. Restrict access to the Shutdown endpoint to the ACTUATOR_ADMIN role.
+     * 2. Allow access to all other actuator endpoints.
+     * 3. Allow access to static resources.
+     * 4. Allow access to the home page (/).
+     * 5. All other requests need to be authenticated.
+     * 5. Enable http basic authentication to make the configuration complete.
+     *    You are free to use any other form of authentication.
      */
 
     @Override
